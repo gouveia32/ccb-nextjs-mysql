27d35c0594edb58bc586b58e4a44874f5a618a50
Jose Alves de Gouveia <gouveia32@gmail.com>
2021-08-26 10:48:15 -0300

    form

--- src/pages/cliente/[id]/edit.js
+++ src/pages/cliente/[id]/edit.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
-import { Button, Form, Loader } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
 
 const Edit = ({ note }) => {
